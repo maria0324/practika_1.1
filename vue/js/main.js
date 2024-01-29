7cf3f2ab77d96fe303b77a2970b80899666c5a86
79234 <marusya0323@gmail.com>
2024-01-29 23:19:13 +0700

    Добавление в объект с данными, используемый при создании экземпляра Vue, нового логического свойства onSale. Оно  указывает на то, проводится ли распродажа. Вычисляемое свойство sale, которое, на основе brand, product и onSale формирует строку, сообщающую о том, проводится ли сейчас распродажа или нет. Вывод этой строки в карточке товара.

--- vue/js/main.js
+++ vue/js/main.js
@@ -2,9 +2,10 @@ let app = new Vue({
     el: '#app',
     data: {
         product: "Socks",
+        brand: 'Vue Mastery',
         image: "./assets/vmSocks-green-onWhite.jpg",
         altText: "A pair of socks",
-        inStock: false,
+        inStock: true,
         details: ['80% cotton', '20% polyester', 'Gender-neutral'],
         variants: [
             {
@@ -18,8 +19,18 @@ let app = new Vue({
                 variantImage: "./assets/vmSocks-blue-onWhite.jpg",
             }
         ],
-        cart: 0
+        cart: 0,
+        onSale : false
     },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product;
+        },
+        sale(){
+            return this.onSale ? `${this.brand} ${this.product} проводит распродажу!` : `${this.brand} ${this.product} не проводит распродажу.`;
+        }
+     },
+     
     methods: {
         addToCart() {
             this.cart += 1
