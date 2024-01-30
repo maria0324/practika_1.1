a461b922082928ac3e0de5590e3d27da5d7f49c9
79234 <marusya0323@gmail.com>
2024-01-30 09:47:00 +0700

    Создайние нового компонента product-details, который  использует входной параметр details и отвечает за визуализацию той части карточки товара, которая раньше формировалась с использованием другого кода

--- vue/js/main.js
+++ vue/js/main.js
@@ -1,43 +1,116 @@
-let app = new Vue({
-    el: '#app',
-    data: {
-        product: "Socks",
-        brand: 'Vue Mastery',
-        image: "./assets/vmSocks-green-onWhite.jpg",
-        altText: "A pair of socks",
-        inStock: true,
-        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
-        variants: [
-            {
-                variantId: 2234,
-                variantColor: 'green',
-                variantImage: "./assets/vmSocks-green-onWhite.jpg",
-            },
-            {
-                variantId: 2235,
-                variantColor: 'blue',
-                variantImage: "./assets/vmSocks-blue-onWhite.jpg",
-            }
-        ],
-        cart: 0,
-        onSale : false
-    },
-    computed: {
-        title() {
-            return this.brand + ' ' + this.product;
-        },
-        sale(){
-            return this.onSale ? `${this.brand} ${this.product} проводит распродажу!` : `${this.brand} ${this.product} не проводит распродажу.`;
-        }
-     },
-     
-    methods: {
-        addToCart() {
-            this.cart += 1
-        },
-        updateProduct(variantImage) {
-            this.image = variantImage
-        }
-    }
- })
+Vue.component('product-details', { 
+    props: { 
+        details: { 
+            type: Array, 
+            required: true 
+        } 
+    }, 
+    template: ` 
+    <ul> 
+        <li v-for="detail in details">{{ detail }}</li> 
+    </ul> 
+    ` 
+}) 
  
+Vue.component('product', { 
+    props: { 
+        premium: { 
+            type: Boolean, 
+            required: true 
+        } 
+    },    
+    template: ` 
+    <div class="product"> 
+        <div class="product-image"> 
+            <img :src="image" :alt="altText"/> 
+        </div> 
+      
+        <div class="product-info"> 
+            <h1>{{ title }}</h1> 
+            <p v-if="inStock">In stock</p> 
+            <p v-else>Out of Stock</p> 
+            <product-details :details="details"></product-details> 
+            <p>User is premium: {{ premium }}</p> 
+            <p>Shipping: {{ shipping }}</p> 
+            <div 
+                    class="color-box" 
+                    v-for="(variant, index) in variants" 
+                    :key="variant.variantId" 
+                    :style="{ backgroundColor:variant.variantColor }" 
+                    @mouseover="updateProduct(index)" 
+            ></div> 
+            </div> 
+      
+            <div class="cart"> 
+                <p>Cart({{ cart }})</p> 
+            </div> 
+             
+            <button 
+                    v-on:click="addToCart" 
+                    :disabled="!inStock" 
+                    :class="{ disabledButton: !inStock }" 
+            > 
+                Add to cart 
+            </button> 
+        </div> 
+    </div> 
+  `, 
+    data() { 
+        return{ 
+            product: "Socks", 
+            brand: 'Vue Mastery', 
+            selectedVariant: 0, 
+            altText: "A pair of socks", 
+            details: ['80% cotton', '20% polyester', 'Gender-neutral'], 
+            variants: [ 
+                { 
+                    variantId: 2234, 
+                    variantColor: 'green', 
+                    variantImage: "./assets/vmSocks-green-onWhite.jpg", 
+                    variantQuantity: 10 
+                }, 
+                { 
+                    variantId: 2235, 
+                    variantColor: 'blue', 
+                    variantImage: "./assets/vmSocks-blue-onWhite.jpg", 
+                    variantQuantity: 0 
+                } 
+            ], 
+            cart: 0 
+        } 
+    }, 
+    methods: { 
+        addToCart() { 
+            this.cart += 1 
+        }, 
+        updateProduct(index) { 
+            this.selectedVariant = index; 
+            console.log(index); 
+        } 
+    }, 
+    computed: { 
+        title() { 
+            return this.brand + ' ' + this.product; 
+        }, 
+        image() { 
+            return this.variants[this.selectedVariant].variantImage; 
+        }, 
+        inStock(){ 
+            return this.variants[this.selectedVariant].variantQuantity 
+        }, 
+        shipping() { 
+            if (this.premium) { 
+                return "Free"; 
+            } else { 
+                return 2.99 
+            } 
+        }   
+    } 
+}) 
+ 
+let app = new Vue({ 
+    el: '#app', 
+    data: { 
+        premium: true 
+    } 
+})
